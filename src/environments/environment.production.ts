cebe082e01696c56d7bafa2ff3951784b3da529d
WesleyIntexBank <wesley.bezerra@intexbank.com.br>
2026-05-04 13:16:48 -0300

    Ajuste de http para https

--- src/environments/environment.production.ts
+++ src/environments/environment.production.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://api.wevitech.com.br/api'
+  apiUrl: 'https://api.wevitech.com.br/api'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://api.wevitech.com.br/api'
+  apiUrl: 'https://api.wevitech.com.br/api'
 };
